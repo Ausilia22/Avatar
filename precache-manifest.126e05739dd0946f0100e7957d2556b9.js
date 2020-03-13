c27227806810437c0f5d9bd39a44469dec862aa4
Ausilia22 <ausilia.lekstakaj@gmail.com>
2020-03-13 11:29:20 +0100

    Updates

---
+++ precache-manifest.126e05739dd0946f0100e7957d2556b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db86b72d4c81ae08ce03ad93a34451b1",
+    "url": "/Avatar/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/Avatar/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5e833163537553c301d4",
+    "url": "/Avatar/static/css/main.470d82a5.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/Avatar/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Avatar/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e833163537553c301d4",
+    "url": "/Avatar/static/js/main.3c101dce.chunk.js"
+  },
+  {
+    "revision": "cafb3df811206fda6f4f",
+    "url": "/Avatar/static/js/runtime-main.2674ffbf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Avatar/static/media/SEGA.12a3db8b.woff"
+  }
+]);
